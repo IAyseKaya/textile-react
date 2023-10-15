a859da528eecbbfd488750b1c0005023399e5b77
IAyseKaya <i.ayse-kaya@hotmail.com>
2023-10-15 13:19:45 +0300

    added navbar and tailwind

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from "./componentTextile/navbar/navbar.jsx";
+import Footer from "./componentTextile/footer/Footer.jsx";
+import MainPage from "./MainPage/MainPage.jsx";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Ayşe Kaya <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+
+  return ( 
+  <div
+    id="body"
+    >
+       <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route index element={<MainPage />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/MainPage/MainPage.jsx
@@ -0,0 +1,9 @@
+function MainPage() {
+    return(
+        <div className="main h-24 bg-red-500">
+            
+        </div>
+    );
+}
+
+export default MainPage;
